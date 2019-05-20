5ffe2498293a300712830c128265c3b766f4ce08
HoaTD <trandinhhoadev@gmail.com>
2019-05-20 19:09:22 +0700

    lớp API để làm ajax

---
+++ Hospital/src/main/java/com/hospital/Controller/APIController.java
@@ -0,0 +1,10 @@
+package com.hospital.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api/")
+public class APIController {
+
+}
